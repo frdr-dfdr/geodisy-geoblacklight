79c4ef6bb926b3d49b61191002054a54f7f42788
Joel Farthing <joel.farthing@computecanada.ca>
2020-04-29 19:50:03 +0000

    Changed 'More details at' to 'Data source' on record page

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -143,7 +143,7 @@ class CatalogController < ApplicationController
     config.add_show_field Settings.FIELDS.PROVENANCE, label: 'Held by', link_to_facet: true
     config.add_show_field(
       Settings.FIELDS.REFERENCES,
-      label: 'More details at',
+      label: 'Data source',
       accessor: [:external_url],
       if: proc { |_, _, doc| doc.external_url },
       helper_method: :render_references_url
