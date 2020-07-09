630b9350a57244a99509c84ea5d294c1fa735ded
Paul Dante <pdante@Paul-Desky>
2020-07-09 03:42:31 -0700

    New attempt to adjust map starting location

--- app/assets/javascripts/geoblacklight/modules/home.js
+++ app/assets/javascripts/geoblacklight/modules/home.js
@@ -7,11 +7,12 @@ Blacklight.onLoad(function() {
     geoblacklight.map.addControl(L.control.geosearch({
       baseUrl: data.catalogPath,
       dynamic: false,
+      zoom: 8,
+      center: [40.737, -73.923],
       searcher: function() {
         window.location.href = this.getSearchUrl();
       },
       staticButton: '<a class="btn btn-primary">Search here</a>'
     }));
-    geoblacklight.map.setView(new L.LatLng(40.737, -73.923), 8);
   });
 });
