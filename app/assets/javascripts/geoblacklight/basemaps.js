b138ae8194fe2944e87dddcd15074798f2414f31
Paul Dante <paul.dante@ubc.ca>
2020-10-08 15:55:40 -0700

    Move Leaflet attribution to footer

--- app/assets/javascripts/geoblacklight/basemaps.js
+++ app/assets/javascripts/geoblacklight/basemaps.js
@@ -91,7 +91,7 @@ GeoBlacklight.Basemaps = {
       }
     ),
     Esri_WorldTopoMap: L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}', {
-    	attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community',
+    	attribution: 'Tiles &copy; Esri&#185;',
     	worldCopyJump: true,
     	maxZoom: 16,
     	retina: '@2x',
