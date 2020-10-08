edb5fe29df8a5da55f0e5bedb3a722da0ff58c4c
Paul Dante <paul.dante@ubc.ca>
2020-10-08 10:14:17 -0700

    Removed extra semicolon

--- app/assets/javascripts/geoblacklight/basemaps.js
+++ app/assets/javascripts/geoblacklight/basemaps.js
@@ -90,7 +90,8 @@ GeoBlacklight.Basemaps = {
         detectRetina: false
       }
     ),
-   Esri_WorldTopoMap: L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}', {
+    Esri_WorldTopoMap: L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}', {
     	attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community'
-    });
+      }
+    )
 };
