a1953ce5a1e9b9d6dcaad8d1c3edbb78fee5af14
Paul Dante <paul.dante@ubc.ca>
2020-10-26 10:46:25 -0700

    Trying to change default map zoom

--- app/assets/javascripts/geoblacklight/basemaps.js
+++ app/assets/javascripts/geoblacklight/basemaps.js
@@ -91,7 +91,11 @@ GeoBlacklight.Basemaps = {
       }
     ),
     Esri_WorldTopoMap: L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}', {
-    	attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community'
-      }
-    )
+            attribution: 'Tiles &copy; Esri et al. (1)',
+            worldCopyJump: true,
+            maxZoom: 16,
+            retina: '@2x',
+            detectRetina: false,
+          }
+        )
 };
