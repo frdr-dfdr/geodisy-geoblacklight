ef9f052ff715023f698cbd2c6dc3934fce64c868
Joel Farthing <joel.f.farthing@gmail.com>
2020-12-01 19:42:01 -0600

    add assets:precompile command to entrypoint script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
-################################################################################
-# This Script is taken right from the Docker documentation page at :           #
-# https://docs.docker.com/compose/rails/                                       #
-################################################################################
 
 set -e
+
+################################################################################
+# Compile the Rails assets at runtime. They will be shared in a volume with    #
+# the Nginx container                                                          #
+################################################################################
+bundle exec rake assets:precompile
+
+################################################################################
+# The steps below are taken from the Docker documentation page at :            #
+# https://docs.docker.com/compose/rails/  Dec 1, 2020                          #
+################################################################################
 
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /myapp/tmp/pids/server.pid
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
+
 
