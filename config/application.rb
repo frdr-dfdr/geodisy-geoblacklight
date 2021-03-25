9bda1f1f11ca8a1f9bc23b6f97b2cc4d17c13403
Paul Dante <paul.dante@ubc.ca>
2021-03-24 22:32:51 -0700

    Setting up localization toggle in navbar

--- config/application.rb
+++ config/application.rb
@@ -25,5 +25,12 @@ module Geodisy
         resource '*', headers: :any, methods: [:get, :post, :put, :delete, :options]
       end
     end
+    # Where the I18n library should search for translation files
+    I18n.load_path += Dir[Rails.root.join('lib', 'locale', '*.{rb,yml}')]
+    # Permitted locales available for the application
+    I18n.available_locales = [:en, :fr]
+
+    # Set default locale to something other than :en
+    I18n.default_locale = :en
   end
 end
