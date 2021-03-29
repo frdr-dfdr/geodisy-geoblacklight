d3c8e11546bf73ebc67b6980b6c1992bdb9a88a0
Adam McKenzie <adam.mckenzie@usask.ca>
2021-03-29 14:47:21 -0600

    Do translation replacements after page load

--- app/assets/javascripts/geoblacklight/modules/geosearch_scripts.js
+++ app/assets/javascripts/geoblacklight/modules/geosearch_scripts.js
@@ -1,19 +1,5 @@
 $( document ).ready(function() {
-    console.log( "ready!" );
-});
-
-$(document).on('turbolinks:load', function() {
-    console.log( "turbo links load" );
-});
-
-$(document).on('turbolinks:load', function() {
     $('.redo').text(I18n.t('map.redo_search'));
-});
-
-$(document).on('turbolinks:load', function() {
     $('.move_map').text(I18n.t('map.move_map_search'));
-});
-
-$(document).on('turbolinks:load', function() {
     $('.search_here').text(I18n.t('map.search_here'));
 });
