b87763426a2b8a6235af867dd20f40bea54e4f51
Joel Farthing <joel.f.farthing@gmail.com>
2021-04-19 17:11:50 -0600

    correct classnames

--- app/assets/javascripts/geoblacklight/modules/geosearch_scripts.js
+++ app/assets/javascripts/geoblacklight/modules/geosearch_scripts.js
@@ -1,5 +1,5 @@
 $( document ).ready(function() {
     $('.redo').text(I18n.t('map.redo_search'));
-    $('.move_map').text(I18n.t('map.move_map_search'));
-    $('.search_here').text(I18n.t('map.search_here'));
+    $('.move-map').text(I18n.t('map.move_map_search'));
+    $('.search-here').text(I18n.t('map.search_here'));
 });
