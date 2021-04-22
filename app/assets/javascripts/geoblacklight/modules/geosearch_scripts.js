6393a761cfb315a632843bb15f4110dcc5d75826
Joel Farthing <joel.f.farthing@gmail.com>
2021-04-22 15:35:21 -0600

    fix search here button

--- app/assets/javascripts/geoblacklight/modules/geosearch_scripts.js
+++ app/assets/javascripts/geoblacklight/modules/geosearch_scripts.js
@@ -2,6 +2,5 @@ $( document ).ready(function() {
     $('.redo').text(I18n.t('map.redo_search'));
     $('.move_map').text(I18n.t('map.move_map_search'));
     //$('.search_here').text(I18n.t('map.search_here'));
-    $('.search_here').html('<a href="#" class="btn btn-primary">'+I18n.t('map.search_here')+'</a>');
-
+    $('.search_here > .btn').text(I18n.t('map.search_here'));
 });
