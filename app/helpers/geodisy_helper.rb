a1a4382dfada93fa6506ee4501d2884bd43f3d11
Paul Dante <paul.dante@ubc.ca>
2021-04-27 20:16:15 -0700

    Removed debugging loggers

--- app/helpers/geodisy_helper.rb
+++ app/helpers/geodisy_helper.rb
@@ -4,12 +4,9 @@ module GeodisyHelper
   # @return [String]
   def geoblacklight_basemap
 
-    Rails.logger.error(I18n.locale)
     if I18n.locale == :fr then
-      Rails.logger.error("OpenStreetMap_France")
       return "OpenStreetMap_France"
     else
-      Rails.logger.error("Esri_WorldTopoMap")
       return "Esri_WorldTopoMap"
     end
   end
